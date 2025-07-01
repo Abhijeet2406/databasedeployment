4d3350c78d5d422797f557ade7170f6de7874ebe
Abhijeet2406 <test@att.com>
2025-07-02 01:14:54 +0530

    Backup PROCEDURE dbo.usp_AddEmployee

--- procedures/dbo.usp_AddEmployee.sql
+++ procedures/dbo.usp_AddEmployee.sql
@@ -1,8 +1,10 @@
--- Auto-generated backup for dbo.usp_AddEmployee from SQLServer on 2025-07-02 01:03:28
-CREATE   PROCEDURE [dbo].[usp_AddEmployee]
+-- Auto-generated backup for dbo.usp_AddEmployee from SQLServer on 2025-07-02 01:14:53
+
+-- Sample Procedure
+CREATE PROCEDURE usp_AddEmployee
     @Name NVARCHAR(100),
     @Department NVARCHAR(100)
 AS
 BEGIN
-    INSERT INTO Employees1 (Name, Department) VALUES (@Name, @Department);
+    INSERT INTO Employees (Name, Department) VALUES (@Name, @Department);
 END;
