eba74adc590f7666bd000251466be810b888b238
Abhijeet2406 <test@att.com>
2025-07-02 00:08:07 +0530

    Backup PROCEDURE dbo.usp_AddEmployee

--- procedures/dbo.usp_AddEmployee.sql
+++ procedures/dbo.usp_AddEmployee.sql
@@ -1,4 +1,4 @@
--- Auto-generated backup for dbo.usp_AddEmployee from SQLServer on 2025-07-01 23:55:08
+-- Auto-generated backup for dbo.usp_AddEmployee from SQLServer on 2025-07-02 00:08:07
 
 -- Sample Procedure
 CREATE PROCEDURE usp_AddEmployee
