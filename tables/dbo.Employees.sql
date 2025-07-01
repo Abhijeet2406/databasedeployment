f6ff5a43d06c490c46fbe3ff69d55e730aa5eda4
Abhijeet2406 <test@att.com>
2025-07-02 00:08:06 +0530

    Backup table dbo.Employees

--- tables/dbo.Employees.sql
+++ tables/dbo.Employees.sql
@@ -1,9 +1,8 @@
--- Simplified structure backup for dbo.Employees created on 2025-07-01
+-- Simplified structure backup for dbo.Employees created on 2025-07-02
 -- NOTE: Constraints, Indexes, and other properties are not included.
 CREATE TABLE dbo.Employees (
     [EmployeeID] INT,
     [Name] NVARCHAR(100),
     [Department] NVARCHAR(100),
-    [CreatedDate] DATETIME,
-    [Flag] VARCHAR(1)
+    [CreatedDate] DATETIME
 );
