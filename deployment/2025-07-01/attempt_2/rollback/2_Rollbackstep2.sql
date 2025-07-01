472ead86ea7899b82df4aa1bdf8f6bf648401ee0
Abhijeet2406 <test@att.com>
2025-07-02 01:26:56 +0530

    Rollback script 2_Rollbackstep2.sql

--- deployment/2025-07-01/attempt_2/rollback/2_Rollbackstep2.sql
+++ deployment/2025-07-01/attempt_2/rollback/2_Rollbackstep2.sql
@@ -1,8 +1,1 @@
-CREATE OR ALTER PROCEDURE usp_AddEmployee
-    @Name NVARCHAR(100),
-    @Department NVARCHAR(100)
-AS
-BEGIN
-    INSERT INTO Employees (Name, Department) VALUES (@Name, @Department);
-END;
-GO
+CREATE OR ALTER PROCEDURE usp_AddEmployee     @Name NVARCHAR(100),     @Department NVARCHAR(100) AS BEGIN     INSERT INTO Employees (Name, Department) VALUES (@Name, @Department) END 
