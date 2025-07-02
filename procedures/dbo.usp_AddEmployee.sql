5db30aa3b40241a010b278da655599610e98c0f7
Abhijeet2406 <av6828@att.com@att.com>
2025-07-02 14:06:44 +0530

    Backup PROCEDURE dbo.usp_AddEmployee

--- procedures/dbo.usp_AddEmployee.sql
+++ procedures/dbo.usp_AddEmployee.sql
@@ -1,2 +1,8 @@
--- Auto-generated backup for dbo.usp_AddEmployee from SQLServer on 2025-07-02 01:26:50
-CREATE   PROCEDURE usp_AddEmployee     @Name NVARCHAR(100),     @Department NVARCHAR(100) AS BEGIN     INSERT INTO Employees (Name, Department) VALUES (@Name, @Department) END 
+-- Auto-generated backup for dbo.usp_AddEmployee from SQLServer on 2025-07-02 14:06:43
+CREATE   PROCEDURE dbo.usp_AddEmployee
+    @Name NVARCHAR(100),
+    @Department NVARCHAR(100)
+AS
+BEGIN
+    INSERT INTO Employees (Name, Department) VALUES (@Name, @Department);
+END
